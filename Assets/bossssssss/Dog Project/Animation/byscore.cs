2000af30604e78b9ef9778624dd1461be70e62b1
WaltherP380 <130338827+WaltherP380@users.noreply.github.com>
2025-09-28 23:49:35 +0700

    dog update โง่ๆ 0.1
    อันนี้ทำทิ้้งไว้ก่อนนะ เดะมาแก้อนิเมชั่นให้มันดีๆ

--- Assets/bossssssss/Dog Project/Animation/byscore.cs
+++ Assets/bossssssss/Dog Project/Animation/byscore.cs
@@ -9,16 +9,23 @@ public class Playtestanim : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         if(score == 200){
-            anim.SetTrigger("Open1");
-        }else if(score == 500){
-            anim.SetBool("open2",true);
+        if (score == 200)
+        {
+            anim.SetTrigger("1");   
+        }
+        else if (score == 500)
+        {
+            anim.SetTrigger("2");
+        }
+        else if (score == 800)
+        {
+            anim.SetTrigger("3");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 }
